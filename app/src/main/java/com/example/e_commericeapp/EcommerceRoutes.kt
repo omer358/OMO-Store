7c2f0e91f4080ce059cd2be3f3e6f9111465285b
omer358 <omermaki358@gmail.com>
2024-01-11 11:19:05 +0200

    initialize routes

---
+++ app/src/main/java/com/example/e_commericeapp/EcommerceRoutes.kt
@@ -0,0 +1,8 @@
+package com.example.e_commericeapp
+
+const val SPLASH_SCREEN = "Splash Screen"
+const val HOME_SCREEN = "Home Screen"
+const val CART_SCREEN = "Cart Screen"
+const val WISH_LIST_SCREEN = "WishList Screen"
+const val LOGIN_SCREEN = "Login Screen"
+const val SIGN_UP_SCREEN = "SignUp Screen"
